516b0babc305bfcf277f272f8b2eb3c700f83fee
Pedro Mota <83295376+PedrooMota@users.noreply.github.com>
2024-02-27 17:26:33 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Provider as ReduxProvider } from 'react-redux'
+import { store } from './store';
+import { Player } from './pages/player';
+
+import './styles/global.css'
+
+export function App() {
+
+  return (
+    <ReduxProvider store={store}>
+      <Player />
+    </ReduxProvider>
+  )
+}
+
